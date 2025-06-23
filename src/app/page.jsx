ae2336cc3835e5c3aa01b4aa2980b09e4b72d109
digmen <digmen68@gmail.com>
2025-06-23 15:28:56 +0600

    v3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,19 @@ export default function page() {
   return (
     <main className='mx-[93px] mt-[8px]'>
       <article
-        className='bg-[url("/images/hero/hero.png")] bg-cover bg-no-repeat w-full h-[952px]'
+        className='bg-[url("/images/hero/hero.png")] bg-cover bg-no-repeat w-full h-screen'
       >
-
+        <aside className='pt-[256px]'>
+          <h2>
+            Where others saw limits, we saw room to create differently — and better.
+          </h2>
+          <p>
+            We built a full-cycle agency that merges strategy, design, technology, and marketing into one seamless experience
+          </p>
+          <p>
+            Partner with us to unlock bold ideas, ignite innovation, and build brands that move people — and markets.
+          </p>
+        </aside>
       </article>
     </main>
   )
